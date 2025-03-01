333c5a57a730298eea6ab288814cf5731c78e971
Mati <you@example.com>
2025-03-01 17:00:23 +0200

    added temperature and humidity sensor

--- pm2.5_air_quality.ino
+++ pm2.5_air_quality.ino
@@ -1,16 +1,17 @@
-int ledPin = 7;  // Контролен пин за LED в сензора
-int sensorPin = A0;  // Аналогов вход за измерване на прах
+#include <DHT.h>
 
-int redPin = 9;   // Червен канал
-int greenPin = 10; // Зелен канал
-int bluePin = 11;  // Син канал
+int ledPin = 4;    // Контролен пин за LED в сензора
+int sensorPin = A0; // Аналогов вход за измерване на прах
+
+#define DHTPIN 11
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   Serial.begin(9600);
   pinMode(ledPin, OUTPUT);
-  pinMode(redPin, OUTPUT);
-  pinMode(greenPin, OUTPUT);
-  pinMode(bluePin, OUTPUT);
+  dht.begin();
 }
 
 float readDustLevel() {
@@ -28,51 +29,28 @@ float readDustLevel() {
   return dustDensity;
 }
 
-void updateLED(float dustLevel) {
-  if (dustLevel < 15) {      
-    analogWrite(redPin, 0);
-    analogWrite(greenPin, 63);
-    analogWrite(bluePin, 0);
-    Serial.println("Excellent");  
-  } 
-  else if (dustLevel < 40) { 
-    analogWrite(redPin, 63);
-    analogWrite(greenPin, 15);
-    analogWrite(bluePin, 0); 
-    Serial.println("Good");   
-  } 
-  else if (dustLevel < 65) { 
-    analogWrite(redPin, 63);
-    analogWrite(greenPin, 5);
-    analogWrite(bluePin, 0); 
-    Serial.println("Mild");  
-  } 
-  else if (dustLevel < 150) {                      
-    analogWrite(redPin, 63);
-    analogWrite(greenPin, 0);
-    analogWrite(bluePin, 0);  
-    Serial.println("Moderate");
-  }
-  else if (dustLevel < 250) {                      
-    analogWrite(redPin, 63);
-    analogWrite(greenPin, 0);
-    analogWrite(bluePin, 20);
-    Serial.println("Severe");
-  }
-  else {                      
-    analogWrite(redPin, 6);
-    analogWrite(greenPin, 0);
-    analogWrite(bluePin, 2);
-    Serial.println("Serious");
-  }
-}
 void loop() {
   float dustLevel = readDustLevel();
-  Serial.print("Dust Density: ");
+  float temp = dht.readTemperature(); // Чете температура (по Целзий)
+  float hum = dht.readHumidity();     // Чете влажност
+  Serial.print("Dust Density: ");        
   Serial.print(dustLevel);
   Serial.println(" µg/m³");
+  
 
-  updateLED(dustLevel);  
+  delay(3000); // Намаляваме забавянето
 
-  delay(3000);
+
+if (isnan(temp) || isnan(hum)) {
+    Serial.println("Error");
+    return;
+  }
+
+  Serial.print("TEMP: ");
+  Serial.print(temp);
+  Serial.print("°C | HUM: ");
+  Serial.print(hum);
+  Serial.println("%");
+
+   delay(1000); // Чака 2 секунди между измерванията
 }
